0997bef27ec4c08f47bd997f35a8f332928af7ab
Maxim Varfolomeyev <varfick@gmail.com>
2020-03-24 18:31:09 +0300

    fixes #002 (adds support for Nether Wart)

---
+++ src/main/java/ru/maxvar/mcf/reap/mixins/NetherWartBlockMixin.java
@@ -0,0 +1,59 @@
+package ru.maxvar.mcf.reap.mixins;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.NetherWartBlock;
+import net.minecraft.block.PlantBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.DefaultedList;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ItemScatterer;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.List;
+
+@SuppressWarnings("unused")
+@Mixin(NetherWartBlock.class)
+public abstract class NetherWartBlockMixin extends PlantBlock {
+
+    protected NetherWartBlockMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Shadow
+    public static final IntProperty AGE = Properties.AGE_3;
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient() & state.get(AGE) >= 3) {
+            List<ItemStack> dropList = getDroppedStacks(state, (ServerWorld) world, pos, null, player, player.getStackInHand(hand));
+            DefaultedList<ItemStack> drops = DefaultedList.of();
+            drops.addAll(dropList);
+
+            for (ItemStack stack : drops) {
+                if (stack.getItem() == Items.NETHER_WART) {
+                    ItemStack seedStack = stack.copy();
+                    drops.remove(stack);
+                    seedStack.decrement(1);
+                    drops.add(seedStack);
+                    break;
+                }
+            }
+
+            world.setBlockState(pos, state.with(AGE, 0));
+            ItemScatterer.spawn(world, pos, drops);
+        }
+        return super.onUse(state, world, pos, player, hand, hit);
+    }
+
+}
