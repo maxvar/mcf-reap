2498f6f3f9191eabf8cd1673ed45f8b97b5969f9
Maxim Varfolomeyev <varfick@gmail.com>
2021-06-21 00:48:25 +0300

    added logic to collect into players inventory in CocoaBlockMixin (now always)

--- src/main/java/ru/maxvar/mcf/reap/mixins/CocoaBlockMixin.java
+++ src/main/java/ru/maxvar/mcf/reap/mixins/CocoaBlockMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.CocoaBlock;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
@@ -47,7 +48,22 @@ public abstract class CocoaBlockMixin extends HorizontalFacingBlock {
                 }
             }
             world.setBlockState(pos, state.with(CocoaBlock.AGE, 0));
-            ItemScatterer.spawn(world, pos, drops);
+            //TODO scatter or put loot into players inventory immediately (based on config)
+            DefaultedList<ItemStack> remainingDrops;
+            if (2 * 2 == 4) {
+                //collect directly while there is room in player's inventory
+                PlayerInventory playerInventory = player.getInventory();
+                for (ItemStack stack : drops) {
+                    playerInventory.insertStack(stack);
+                }
+                remainingDrops = DefaultedList.of();
+                for (ItemStack stack : drops) if (stack.getCount() > 0) remainingDrops.add(stack);
+            } else {
+                //skip collection
+                remainingDrops = drops;
+            }
+            //scatter remaining drops
+            ItemScatterer.spawn(world, pos, remainingDrops);
             return ActionResult.SUCCESS;
         }
         return super.onUse(state, world, pos, player, hand, hit);
