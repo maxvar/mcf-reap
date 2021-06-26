fce51aef51179d3f518f7c8da8004bc04d199a85
Maxim Varfolomeyev <varfick@gmail.com>
2021-06-26 14:18:45 +0300

    version++ always try to collect into players inventory extracted all reaping logic into ReapHelper removed CropsInfo (the seed for CropsBlocks can be obtained without)

--- src/main/java/ru/maxvar/mcf/reap/mixins/CropBlockMixin.java
+++ src/main/java/ru/maxvar/mcf/reap/mixins/CropBlockMixin.java
@@ -4,21 +4,17 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
 import net.minecraft.block.PlantBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.Hand;
-import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import ru.maxvar.mcf.reap.CropsInfo;
-
-import java.util.List;
+import ru.maxvar.mcf.reap.ReapHelper;
 
 import static net.minecraft.block.CropBlock.AGE;
 
@@ -30,7 +26,7 @@ public abstract class CropBlockMixin extends PlantBlock {
         super(settings);
     }
 
-    @SuppressWarnings({"SameReturnValue", "unused"})
+    @SuppressWarnings("SameReturnValue")
     @Shadow
     public boolean isMature(BlockState state) {
         return true;
@@ -42,27 +38,17 @@ public abstract class CropBlockMixin extends PlantBlock {
         return AGE;
     }
 
+    @SuppressWarnings("SameReturnValue")
+    @Shadow
+    public Item getSeedsItem() {
+        return Items.WHEAT_SEEDS;
+    }
+
     @SuppressWarnings("deprecation")
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (!world.isClient() & isMature(state)) {
-            List<ItemStack> dropList = getDroppedStacks(state, (ServerWorld) world, pos, null, player, player.getStackInHand(hand));
-            DefaultedList<ItemStack> drops = DefaultedList.of();
-            drops.addAll(dropList);
-
-            for (ItemStack stack : drops) {
-                if (stack.getItem() == CropsInfo.getSeedsItem(this)) {
-                    ItemStack seedStack = stack.copy();
-                    drops.remove(stack);
-                    seedStack.decrement(1);
-                    drops.add(seedStack);
-                    break;
-                }
-            }
-
-            world.setBlockState(pos, state.with(this.getAgeProperty(), 0));
-            ItemScatterer.spawn(world, pos, drops);
-            return ActionResult.SUCCESS;
+            return ReapHelper.reap(state, world, pos, player, hand, getSeedsItem(), getAgeProperty());
         }
         return super.onUse(state, world, pos, player, hand, hit);
     }
--- src/main/java/ru/maxvar/mcf/reap/mixins/NetherWartBlockMixin.java
+++ src/main/java/ru/maxvar/mcf/reap/mixins/NetherWartBlockMixin.java
@@ -4,22 +4,17 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.NetherWartBlock;
 import net.minecraft.block.PlantBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.Hand;
-import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-
-import java.util.List;
+import ru.maxvar.mcf.reap.ReapHelper;
 
 @SuppressWarnings("unused")
 @Mixin(NetherWartBlock.class)
@@ -36,21 +31,7 @@ public abstract class NetherWartBlockMixin extends PlantBlock {
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (!world.isClient() & state.get(AGE) >= 3) {
-            List<ItemStack> dropList = getDroppedStacks(state, (ServerWorld) world, pos, null, player, player.getStackInHand(hand));
-            DefaultedList<ItemStack> drops = DefaultedList.of();
-            drops.addAll(dropList);
-            for (ItemStack stack : drops) {
-                if (stack.getItem() == Items.NETHER_WART) {
-                    ItemStack seedStack = stack.copy();
-                    drops.remove(stack);
-                    seedStack.decrement(1);
-                    drops.add(seedStack);
-                    break;
-                }
-            }
-            world.setBlockState(pos, state.with(AGE, 0));
-            ItemScatterer.spawn(world, pos, drops);
-            return ActionResult.SUCCESS;
+            return ReapHelper.reap(state, world, pos, player, hand, Items.NETHER_WART, AGE);
         }
         return super.onUse(state, world, pos, player, hand, hit);
     }
